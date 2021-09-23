cf7ffc282476e67043efc10007b83895a8e1ffca
Ruiskey <1830369287@qq.com>
2021-09-23 22:26:48 +0800

    P43 Vuex配置

--- my-vhr-web-design/src/main.js
+++ my-vhr-web-design/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
+import store from './store'
 
 import {postRequest} from "./utils/api";
 import {postKeyValueRequest} from "./utils/api";
@@ -21,5 +22,6 @@ Vue.use(ElementUI);
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
