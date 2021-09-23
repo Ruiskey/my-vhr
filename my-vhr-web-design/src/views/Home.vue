cc07f1ab82daad66745ae5a2caa86182f85a4b1f
Ruiskey <1830369287@qq.com>
2021-09-23 13:18:22 +0800

    p38 Home页title制作

--- my-vhr-web-design/src/views/Home.vue
+++ my-vhr-web-design/src/views/Home.vue
@@ -5,18 +5,58 @@
         <div class="title">微人事</div>
         <el-dropdown class="userInfo" @command="commandHandler">
           <span class="el-dropdown-link">
-            {{ user.name }}<i class="el-icon-arrow-down el-icon--right"></i>
+            {{ user.name }}<i><img :src="user.userface" alt=""/></i>
           </span>
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item command="userinfo">个人中心</el-dropdown-item>
             <el-dropdown-item command="setting">设置</el-dropdown-item>
-<!--            <el-dropdown-item disabled>双皮奶</el-dropdown-item>-->
+            <!--            <el-dropdown-item disabled>双皮奶</el-dropdown-item>-->
             <el-dropdown-item divided command="logout">注销登录</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
+<!--        <el-aside width="200px">Aside</el-aside>-->
+        <el-aside width="200px">
+          <h5>自定义颜色</h5>
+          <el-menu
+              background-color="#545c64"
+              text-color="#fff"
+              active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+<!--              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>-->
+            </el-submenu>
+<!--            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>-->
+          </el-menu>
+        </el-aside>
         <el-main>Main</el-main>
       </el-container>
     </el-container>
@@ -78,5 +118,16 @@ export default {
 
 .homeHeader .userInfo {
   cursor: pointer;
+}
+
+.el-dropdown-link img {
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
+  margin-left: 8px;
+}
+.el-dropdown-link {
+  display: flex;
+  align-items: center;
 }
 </style>
