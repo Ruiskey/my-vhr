f96a853eb41d799a58f265b975a29943f4f39b68
Ruiskey <1830369287@qq.com>
2021-09-26 16:58:58 +0800

    P55 前后端接口对接，新增、删除、查询

--- my-vhr-web-design/src/components/sys/basic/PosMag.vue
+++ my-vhr-web-design/src/components/sys/basic/PosMag.vue
@@ -6,9 +6,10 @@
           class="addPosInput"
           placeholder="添加职位..."
           prefix-icon="el-icon-plus"
+          @keydown.enter.native="addPosition"
           v-model="pos.name">
       </el-input>
-      <el-button icon="el-icon-plus" size="small" style="background-color: #42b983">添加</el-button>
+      <el-button icon="el-icon-plus" size="small" style="background-color: #42b983" @click="addPosition">添加</el-button>
     </div>
     <div class="posMagMain">
       <el-table
@@ -17,6 +18,11 @@
           border
           stripe
           style="width: 50%">
+        <el-table-column
+            type="selection"
+            width="55">
+
+        </el-table-column>
         <el-table-column
             prop="id"
             label="编号"
@@ -29,8 +35,22 @@
         </el-table-column>
         <el-table-column
             prop="createDate"
-            label="创建时间"
-            width="120">
+            label="创建时间">
+        </el-table-column>
+        <el-table-column
+            prop="operate"
+            label="操作">
+          <template slot-scope="scope">
+            <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)">Edit
+            </el-button>
+            <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">Delete
+            </el-button>
+          </template>
         </el-table-column>
       </el-table>
     </div>
@@ -47,16 +67,63 @@ export default {
       },
       position: []
     }
+  },
+  mounted() {
+    this.initPositions();
+  },
+  methods: {
+    initPositions() {
+      this.getRequest("/system/basic/pos/").then(resp => {
+        if (resp) {
+          this.position = resp;
+        }
+      })
+    },
+    handleEdit(index, data) {
+
+    },
+    handleDelete(index, data) {
+      this.$confirm('此操作将永久删除' + data.name + '职位, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.deleteRequest("/system/basic/pos/"+data.id).then(resp=>{
+          if (resp) {
+            this.initPositions();
+          }
+      })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+    addPosition() {
+      if (this.pos.name) {
+        this.postRequest("/system/basic/pos/", this.pos).then(resp => {
+          if (resp) {
+            //重新加载数据 刷新表格
+            this.initPositions();
+            this.pos.name = '';
+          }
+        })
+      } else {
+        this.$message.error("职位名称不可以为空！");
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
-  .addPosInput {
-    width:400px;
-    margin-right: 5px
-  }
-  .posMagMain {
-    margin-top: 7px;
-  }
+.addPosInput {
+  width: 400px;
+  margin-right: 5px
+}
+
+.posMagMain {
+  margin-top: 7px;
+}
 </style>
