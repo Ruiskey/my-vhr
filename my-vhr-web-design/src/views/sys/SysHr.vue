f19d53b3bb1c24b1f5f4c3a5389bb616b7f65209
Ruiskey <1830369287@qq.com>
2021-09-29 22:13:20 +0800

    P77 操作员管理页面展示 P78 用户角色更新操作

--- my-vhr-web-design/src/views/sys/SysHr.vue
+++ my-vhr-web-design/src/views/sys/SysHr.vue
@@ -1,15 +1,107 @@
 <template>
   <div>
-    操作员管理
+    <div style="margin-top: 10px; display: flex; justify-content: center">
+      <el-input v-model="keywords" placeholder="通过用户名搜索用户.." prefix-icon="el-icon-search"
+                style="width: 500px; margin-right: 10px">
+      </el-input>
+      <el-button icon="el-icon-search" type="primary">搜索</el-button>
+    </div>
+    <div class="hr-container">
+      <el-card class="hr-card" v-for="(hr,index) in hrs" :key="index">
+        <div slot="header" class="clearfix">
+          <span>{{hr.name}}</span>
+          <el-button style="float: right; padding: 3px 0; color: red" type="text" icon="el-icon-delete"></el-button>
+        </div>
+        <div>
+          <div class="img-container">
+            <img :src="hr.userface" :alt="hr.name" :title="hr.name" class="userface-img"/>
+          </div>
+<!--          存放个人文本信息-->
+          <div class="userinfo-container">
+            <div>用户名: {{hr.name}}</div>
+            <div>手机号码: {{hr.phone}}</div>
+            <div>电话号码: {{hr.telephone}}</div>
+            <div>地址: {{hr.address}}</div>
+            <div>用户状态:
+              <el-switch
+                v-model="hr.enabled"
+                active-text="启用"
+                inactive-text="禁用"
+                @change="enableChange(hr)"
+                active-color="#13ce66"
+                inactive-color="#ff4949">
+            </el-switch>
+            </div>
+            <div>用户角色:
+              <el-tag type="success" style="margin-right: 3px" v-for="(role,indexj) in hr.roles" :key="indexj">
+                {{role.nameZh}}
+              </el-tag>
+              <el-button icon="el-icon-more" type="text"></el-button>
+            </div>
+            <div>
+              备注
+            </div>
+          </div>
+        </div>
+      </el-card>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SysHr"
+  name: "SysHr",
+  data() {
+    return {
+      keywords: '',
+      hrs: []
+    }
+  },
+  mounted() {
+    this.initHrs();
+  },
+  methods: {
+    enableChange(hr) {
+      delete hr.roles;
+      this.putRequest("/system/hr/", hr).then(resp=>{
+        if (resp) {
+          this.initHrs();
+        }
+      })
+    },
+    initHrs() {
+      this.getRequest("/system/hr/").then(resp => {
+        if (resp) {
+          this.hrs = resp;
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.userface-img {
+  width: 72px;
+  height: 72px;
+  border-radius: 72px;
+}
+.hr-card {
+  width: 350px;
+  margin-bottom: 20px;
+}
+.hr-container{
+  margin-top: 30px;
+  display: flex;
+  flex-flow: wrap;
+  justify-content: space-around;
+}
+.img-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.userinfo-container {
+  margin-top: 20px;
+}
 </style>
