bf592e751b6ea54cd3a608c35b34a8c5ee9266a2
Ruiskey <1830369287@qq.com>
2021-09-29 13:12:57 +0800

    P72 部门管理数据展示和搜索

--- my-vhr-web-design/src/components/sys/basic/DepMag.vue
+++ my-vhr-web-design/src/components/sys/basic/DepMag.vue
@@ -1,12 +1,57 @@
 <template>
-  <div>
-    部门管理
+  <div style="width: 500px">
+    <el-input
+        placeholder="请输入部门名称进行搜索..."
+        prefix-icon="el-icon-search"
+        v-model="filterText">
+    </el-input>
+
+    <el-tree
+        class="filter-tree"
+        :data="deps"
+        :props="defaultProps"
+        default-expand-all
+        :filter-node-method="filterNode"
+        ref="tree">
+    </el-tree>
   </div>
 </template>
 
 <script>
 export default {
-  name: "DepMag"
+  name: "DepMag",
+  data() {
+    return {
+      filterText: '',
+      deps: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  watch: {
+    filterText(val) {
+      // 通过ref找到tree这个标识,然后调用其中的filter方法
+      this.$refs.tree.filter(val);
+    }
+  },
+  mounted() {
+    this.initDeps();
+  },
+  methods: {
+    initDeps() {
+      this.getRequest("/system/basic/department/").then(resp=>{
+        if (resp) {
+          this.deps = resp;
+        }
+      })
+    },
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.name.indexOf(value) !== -1;
+    }
+  }
 }
 </script>
 
